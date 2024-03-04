0be2b65602853147ab6b256a3abaf751ceb0fcf8
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-04 17:24:19 -0600

    exported Home component and import Home into index file to render

--- src/Layout/Home.js
+++ src/Layout/Home.js
@@ -1,3 +1,4 @@
+import React from "react"; // import React
 // The Home Screen is the first page the user sees. It is displayed at '/';
 // Has the following features:
 // 1) A Create Deck button is shown, and clicking it brings the user to the Create Deck screen.
@@ -8,3 +9,11 @@
 // Delete Deck Prompt: When the user clicks the Delete button, a warning message is shown and the user can click OK or Cancel
 // If the user clicks OK, the deck is deleted and the deleted deck is no longer visible on the Home Screen.
 // You can use window.confirm() to create the modal dialog shown 'Delete this deck? You will not be able to recover it.'
+
+function Home() {
+    return (
+        <p>This is the Home Screen.</p>
+    );
+}
+
+export default Home;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import Home from "./Home";
 
 function Layout() {
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
+        <Home />
         <NotFound />
       </div>
     </>
