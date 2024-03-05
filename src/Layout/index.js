e0eb186f3060997168341e3883a9a83674410783
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 01:55:07 -0600

    imported Study component and routed it to the main app

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -5,6 +5,7 @@ import { Route, Link, Switch } from "react-router-dom";
 import Home from "./Home"; // import the Home screen
 import CreateDeck from "./CreateDeck"; // import CreateDeck screen
 import Deck from "./Deck"; // import Deck screen
+import Study from "./Study"; // import Study screen
 
 function Layout() {
   return (
@@ -20,6 +21,9 @@ function Layout() {
           </Route>
           <Route exact path="/decks/:deckId">
             <Deck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <Study />
           </Route>
           <NotFound />
         </Switch>
