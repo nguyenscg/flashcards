28a09e609888092524568a21552f5fd3f7a07ac5
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 01:58:06 -0600

    added breadcrumb nav to study component

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -9,9 +9,22 @@
 // if the user does not restart the deck, they should return to the home screen
 // Studying a deck with two or fewer cards should display "Not enough cards" message and a button to add cards to the deck
 import React from "react";
+import { readDeck } from "../utils/api/index"; // import readDeck function
+import { Link } from "react-router-dom"; // import Link element
 
 function Study() {
-    
+
+    return (
+        <div>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                    <li class="breadcrumb-item"><a href="#">{deck.name}</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Study</li>
+                 </ol>
+            </nav>
+        </div>
+    )
 }
 
 export default Study;
