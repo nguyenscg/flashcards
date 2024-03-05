c5ab852514df1192f4f176dd6298f3a0dd73cba5
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 00:53:42 -0600

    import react in deck component and export deck component

--- src/Layout/Deck.js
+++ src/Layout/Deck.js
@@ -5,3 +5,10 @@
 // 3) There is a breadcrumb navigation bar with a link to home / followed by the name of the deck (e.g., Home/React Router).
 // 4) The screen includes the deck name (e.g., "React Router") and deck description (e.g., "React Router is a collection of navigational components that compose declaratively in your application").
 // 5) The screen includes Edit, Study, Add Cards, and Delete buttons. Each button takes the user to a different destination, as follows:
+import React from "react"; // import react
+
+function Deck() {
+
+}
+
+export default Deck;
