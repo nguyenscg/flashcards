986d9c1697afc681397464b40d08b0fbb6c8cb93
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 04:02:54 -0600

    added card style and added a flip button

--- src/Layout/Study.js
+++ src/Layout/Study.js
@@ -40,6 +40,13 @@ function Study() {
                  </ol>
             </nav>
             <h1>Study: {deck.name}</h1>
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">Card 1 of card.length</h5>
+                    <p className="card-text">{deck.description}</p>
+                    <button type="button" className="btn btn-secondary">Flip</button>
+                </div>
+            </div>
         </div>
     )
 }
