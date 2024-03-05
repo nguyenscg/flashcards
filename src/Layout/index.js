4ecf04664d490ef682a23c605375cd29727b0f81
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 00:52:37 -0600

    added routing to main app to route and import Deck screen

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import { Route, Link, Switch } from "react-router-dom";
 import Home from "./Home"; // import the Home screen
 import CreateDeck from "./CreateDeck"; // import CreateDeck screen
-import { Route, Link, Switch } from "react-router-dom"
+import Deck from "./Deck"; // import Deck screen
 
 function Layout() {
   return (
@@ -16,6 +17,9 @@ function Layout() {
           </Route>
           <Route path="/decks/new">
             <CreateDeck />
+          </Route>
+          <Route exact path="/decks/:deckId">
+            <Deck />
           </Route>
           <NotFound />
         </Switch>
