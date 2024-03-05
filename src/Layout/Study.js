57e3aa5114746a3968020ca0bbbf721b83c9098a
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 01:53:59 -0600

    imported react, exported Study component and added psueudo code

---
+++ src/Layout/Study.js
@@ -0,0 +1,17 @@
+// Path sould be /decks/:deckId/study
+// must use readDeck function
+// Breadcrumb nav / links to home / follow by name of the deck being studied, and the text Study
+// deck Title
+// Cards are shown one at a time, front-side first
+// A button at the bottom of each card "flips" it to the other side.
+// After flipping the card, the screen shows a Next button
+// After final card in the deck has been shown, a message (restart prompt) is shown offering the user the opportunity to restart the deck
+// if the user does not restart the deck, they should return to the home screen
+// Studying a deck with two or fewer cards should display "Not enough cards" message and a button to add cards to the deck
+import React from "react";
+
+function Study() {
+    
+}
+
+export default Study;
