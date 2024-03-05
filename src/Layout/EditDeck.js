39c33d7d1882889016784a9e8f8c1f321aa8218a
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 02:14:42 -0600

    added psuedocode to editdeck component and export it

---
+++ src/Layout/EditDeck.js
@@ -0,0 +1,15 @@
+// path: /decks/:deckId/edit
+// use readDeck() function
+// breadcrumb nav
+// breadcrumb nav / home / deck name / Edit Deck
+// displays same form as Create Deck screen, except it is prefilled with infor for the existing deck
+// user can edit and update the form
+// if the user clicks cancel, the user is taken to the Deck screen
+
+import React from "react"; // import react
+
+function EditDeck() {
+
+}
+
+export default EditDeck;
