55ce5caa32096a3991593af8f0b05412b2e72fa7
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 00:09:20 -0600

    imported Route, Link, and Switch from react-router-dom to route the Home and NotFound components

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import Home from "./Home";
+import Home from "./Home"; // import the Home screen
+import { Route, Link, Switch } from "react-router-dom"
 
 function Layout() {
   return (
     <>
       <Header />
       <div className="container">
-        <Home />
-        <NotFound />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <NotFound />
+        </Switch>
       </div>
     </>
   );
