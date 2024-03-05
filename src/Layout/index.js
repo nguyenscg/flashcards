876367f3ffc00064f289ed6bfd35df3d2d7ceca2
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 00:19:58 -0600

    added routing onto main app to route to createdeck component

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
 import Home from "./Home"; // import the Home screen
+import CreateDeck from "./CreateDeck"; // import CreateDeck screen
 import { Route, Link, Switch } from "react-router-dom"
 
 function Layout() {
@@ -12,6 +13,9 @@ function Layout() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/decks/new">
+            <CreateDeck />
           </Route>
           <NotFound />
         </Switch>
