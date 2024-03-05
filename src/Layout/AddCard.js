dc3797ba596050e6d0308ed4ed43211e42b5174c
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 02:18:43 -0600

    added psuedocode to AddCard component and exported the screen

---
+++ src/Layout/AddCard.js
@@ -0,0 +1,15 @@
+// ADD CARD screen allows the user to add a new card to an existing deck
+// path should be: /decks/:deckId/cards/new
+// readDeck function
+// breadcrumb nav with home / deck name / add card
+// screen displays: deck name: Add Card deck title
+// form is shown with the "front" and "back" fields for a new card. both fields use a <textarea> tag that can accommodate multiple lines of text
+// if the user clicks save, new card is created and associated with relevant deck. then the form is cleared and the process for adding a card is restarted
+// if user clicks done, the user is taken to the deck screen
+import React from "react";
+
+function AddCard() {
+
+}
+
+export default AddCard;
