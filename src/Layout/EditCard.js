cb6df282858f62a0f4984f3a517dd89868761a3f
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 02:21:00 -0600

    added psuedocode to EditCard component and exported it

--- src/Layout/EditCard.js
+++ src/Layout/EditCard.js
@@ -1,1 +1,14 @@
-// 
+// path: /decks/:deckId/cards/:cardId/edit
+// must use readDeck()
+// readCard() function too
+// breadcrumb nav bar with link to home/ name of deck which is edited card is a member, and finally edit card: cardId
+// displays the same from Add Card screen, except it is prefilled with information for existing card. It can be edited and updated
+// if user clicks on either save or cancel, the user is taken to the deck screen
+
+import React from "react";
+
+function EditCard() {
+
+}
+
+export default EditCard;
