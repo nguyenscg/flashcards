eb9e01d5b6d64c44ab40d9b546d8a95aa4b1816d
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-05 05:43:45 -0600

    added breadcrumb nav and buttons

--- src/Layout/EditDeck.js
+++ src/Layout/EditDeck.js
@@ -11,6 +11,31 @@ import { Link, useParams, useHistory } from "react-router-dom"; // import Link e
 
 function EditDeck() {
     const { deckId } = useParams();
+
+    return (
+    <div>
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li className="breadcrumb-item"><Link to={`/decks/:deckId`}>{deck.name}</Link></li>
+                <li className="breadcrumb-item active" aria-current="page">Edit Deck</li>
+            </ol>
+        </nav>
+        <h2>Edit Deck</h2>
+        <form>
+            <div className="form-group">
+                <label for="name">Name</label>
+                <textarea id="name" className="form-control" placeholder="Prefilled Deck"></textarea>
+            </div>
+            <div className="form-group">
+                <label for="description">Description</label>
+                <textarea id="description" className="form-control" placeholder="Prefilled Description"></textarea>
+            </div>
+            <button type="button" className="btn btn-secondary mx-1" onClick={handleCancel}>Cancel</button>
+            <button type="button" className="btn btn-primary mx-1">Submit</button>
+        </form>
+    </div>
+    );
 }
 
 export default EditDeck;
