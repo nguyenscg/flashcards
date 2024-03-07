10d571edc23a5bc5a744acd407ec8a5964ee0001
Sophia Nguyen <nguyencsg@gmail.com>
2024-03-07 13:58:11 -0600

    created navigation for deckedit and updated editdeck to render properly

---
+++ src/Components/DeckEditNav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function DeckEditNav({ deck }) {
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item">
+          <Link to="/">
+            <span className="fa-solid fa-home" />
+            Home
+          </Link>
+        </li>
+        <li className="breadcrumb-item">
+          <Link to={`/decks/${deck.id}`}>{deck.name}</Link>
+        </li>
+        <li className="breadcrumb-item active" aria-current="page">
+          Edit Deck
+        </li>
+      </ol>
+    </nav>
+  );
+}
--- src/Components/EditDeck.js
+++ src/Components/EditDeck.js
@@ -1,88 +1,66 @@
-// path: /decks/:deckId/edit
+import React, { useState, useEffect } from "react";
+import { readDeck, updateDeck } from "../utils/api";
+import { useParams, useHistory } from "react-router-dom";
+import DeckEditNav from "./DeckEditNav";
+import DeckForm from "./DeckForm";
 
-import React, { useState, useEffect } from "react"; // import react, useState, useEffect
-import { readDeck, updateDeck } from "../utils/api/index"; // import readDeck function
-import { Link, useParams, useHistory } from "react-router-dom"; // import Link element, useParams hook, useHistory hook;
+export default function DeckEdit() {
+  const deckId = useParams().deckId;
+  const [deck, setDeck] = useState({});
+  const history = useHistory();
+  const initialFormData = {
+    name: "",
+    description: "",
+  };
+  const [formData, setFormData] = useState({ ...initialFormData });
+  //handle changes made to input fields
+  const handleChange = ({ target }) => {
+    setDeck({
+      ...deck,
+      [target.name]: target.value,
+    });
+  };
 
-function EditDeck() {
-    const { deckId } = useParams();
-    const history = useHistory();
-    const initialState = {
-        id: "",
-        name: "",
-        description: "",
+  //handle submission of new form using updateDeck from API
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await updateDeck(deck);
+    history.push(`/decks/${deck.id}`);
+  };
+
+  //load deck using readDeck function
+  useEffect(() => {
+    async function getDeck() {
+      const response = readDeck(deckId);
+      const deckFromAPI = await response;
+      setDeck(deckFromAPI);
     }
-    const [deck, setDeck] = useState(initialState);
-    
-    useEffect(() => {
-        const fetchDeck = async () => {
-            try {
-                const deckData = await readDeck(deckId);
-                setDeck(deckData);
-            }
-            catch(error) {
-                console.log("Error loading deck: ", error);
-            }
-        }
-        fetchDeck();
-    }, [deckId]); // reruns effect when deckId changes
+    getDeck();
+  }, [deckId]);
 
-    // change handler
-    const handleChange = ({ target }) => {
-        setDeck({
-            ...deck,
-            [target.name]: target.value,
-        });
-    }
-
-    // submit handler
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = updateDeck({...deck});
-        history.push(`/decks/${deckId}`);
-        return data;
-    }
-
-    const handleCancel = () => {
-        history.push(`/decks/${deckId}`);
-    }
-
-    return (
-    <div>
-        <nav aria-label="breadcrumb">
-            <ol className="breadcrumb">
-                <li className="breadcrumb-item"><Link to="/">Home</Link></li>
-                <li className="breadcrumb-item"><Link to={`/decks/:deckId`}>{deck.name}</Link></li>
-                <li className="breadcrumb-item active" aria-current="page">Edit Deck</li>
-            </ol>
-        </nav>
-        <h2>Edit Deck</h2>
-        <form onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label for="name">Name</label>
-                <input 
-                    id="name"
-                    name="name" 
-                    className="form-control"
-                    type="text" 
-                    value={deck.name}
-                    onChange={handleChange} />
-            </div>
-            <div className="form-group">
-                <label for="description">Description</label>
-                <textarea 
-                    id="description"
-                    name="description" 
-                    className="form-control" 
-                    value={deck.description}
-                    type="text"
-                    onChange={handleChange}/>
-            </div>
-            <button type="button" className="btn btn-secondary mx-1" onClick={handleCancel}>Cancel</button>
-            <button type="submit" className="btn btn-primary mx-1">Submit</button>
-        </form>
+  return (
+    <div className="deck-edit">
+      <DeckEditNav deck={deck} />
+      <DeckForm
+        formData={deck}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+      <div>
+        <button
+          className="btn btn-secondary m-2"
+          onClick={() => history.push(`/decks/${deck.id}`)}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="btn btn-primary m-2"
+          onClick={handleSubmit}
+        >
+          Save
+        </button>
+      </div>
     </div>
-    );
+  );
 }
-
-export default EditDeck;
